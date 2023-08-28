7edd7ede5df0642e009d84f5f5b8b409a9be8f81
treesofwhite <>
2023-08-28 12:19:32 +0100

    Use icons and clean up design.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,43 @@
 "use client";
 import React, { useRef } from "react";
-
+import {
+  FaSpotify,
+  FaInstagram,
+  FaYoutube,
+  FaSoundcloud,
+  FaTwitter,
+} from "react-icons/fa";
 const navLinks = [
   {
-    title: "Instagram",
-    url: "https://instagram.com",
-    className: "hover:text-pink-700",
-  },
-  {
-    title: "Spotify",
-    url: "https://www.spotify.com",
-    className: "hover:text-green-700",
+    title: "Twitter",
+    url: "https://www.twitter.com",
+    className: "hover:text-cyan-700",
+    icon: FaTwitter,
   },
   {
     title: "Youtube",
     url: "https://www.youtube.com",
     className: "hover:text-red-700",
+    icon: FaYoutube,
+  },
+  {
+    title: "Spotify",
+    url: "https://www.spotify.com",
+    className: "hover:text-green-700",
+    icon: FaSpotify,
+  },
+  {
+    title: "Instagram",
+    url: "https://instagram.com",
+    className: "hover:text-pink-700",
+    icon: FaInstagram,
+  },
+
+  {
+    title: "Soundcloud",
+    url: "https://www.soundcloud.com",
+    className: "hover:text-orange-700",
+    icon: FaSoundcloud,
   },
 ];
 
@@ -25,7 +47,7 @@ export default function Home() {
       className={`bg-[url('/bg.png')]  bg-cover bg-center flex min-h-screen flex-col items-center justify-between py-20   text-white bg-black`}
     >
       <div></div>
-      <div>
+      <div className="mt-8">
         <img
           src="title.svg"
           alt="Vercel Logo"
@@ -42,24 +64,23 @@ export default function Home() {
           draggable={false}
           height={32}
         />
-        <div className="flex items-center justify-center flex-col md:flex-row gap-3 md:gap-9 mt-20 md:mt-8">
+        <div className="flex items-center justify-center flex gap-3 md:gap-12 mt-20 md:mt-8">
           {navLinks.map((link) => (
             <a
               href={link.url}
               className={`text-neutral-50 select-none transition p-1 uppercase font-semibold ${link.className}`}
-              style={{ fontSize: "0.9rem", letterSpacing: "0.2em" }}
               target="_blank"
               draggable={false}
               key={link.title}
             >
-              {link.title}
+              <link.icon className="h-7 w-7"></link.icon>
             </a>
           ))}
         </div>
       </div>
       <a
         href=""
-        className={`group flex items-center hover:text-neutral-400 select-none transition p-1 uppercase font-semibold mx-16 text-center`}
+        className={`group flex items-center hover:text-neutral-400 select-none transition mb-8 md:mb-0 uppercase font-semibold mx-16 text-center`}
         style={{ fontSize: "1.25rem", letterSpacing: "0.2em" }}
         target="_blank"
       >
